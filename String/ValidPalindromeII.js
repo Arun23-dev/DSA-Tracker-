51c468e860f89ff95072bef570c39572dd60aa27
Arun23-dev <arunch23k@gmail.com>
2026-05-04 14:41:33 +0545

    Solved the valid palindrome II

---
+++ String/ValidPalindromeII.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ * 
+ * 
+ */
+
+function isPalindrome(s, left, right) {
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+var validPalindrome = function (s) {
+
+
+    let left = 0;
+    let right = s.length - 1;
+    let leftElement, rightElement;
+
+    while (left < right) {
+
+        if (s[left] === s[right]) {
+            left++;
+            right--;
+
+        }
+        else {
+            // skip on character 
+            return (isPalindrome(s, left + 1, right) ||
+                isPalindrome(s, left, right - 1))
+        }
+
+    }
+    return true;
+
+};
+console.log(validPalindrome("abbda"))
